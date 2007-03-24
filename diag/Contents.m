0319bf5cb7030f96c20c6c3bb92a46775fd04e50
Jarno Vanhatalo <Jarno.Vanhatalo@helsinki.fi>
2007-03-24 11:11:26 +0000

    Initial import

---
+++ diag/Contents.m
@@ -0,0 +1,5 @@
+% THE DIAGNOSTIC TOOLS (/in the diag/ folder):
+% 
+%    BATCH       - Batch MCMC sample chain and evaluate mean/median of batches
+%    THIN        - Delete burn-in and thin in MCMC-chains
+%    BBPRCTILE     - Bayesian bootstrap percentile
