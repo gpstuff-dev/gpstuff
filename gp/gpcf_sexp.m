5852e07f6027e54f6322fd198793d669c13c8727
Jarno Vanhatalo <Jarno.Vanhatalo@helsinki.fi>
2010-08-26 10:34:19 +0000

    - a bug fixed

--- gp/gpcf_sexp.m
+++ gp/gpcf_sexp.m
@@ -44,8 +44,8 @@ function gpcf = gpcf_sexp(do, varargin)
     ip.addParamValue('magnSigma2',[], @(x) isscalar(x) && x>0);
     ip.addParamValue('lengthScale',[], @(x) isvector(x) && all(x>0));
     ip.addParamValue('metric',[], @isstruct);
-    ip.addParamValue('magnSigma2_prior',[], @(x) isstruct(x) || isempty(x));
-    ip.addParamValue('lengthScale_prior',[], @(x) isstruct(x) || isempty(x));
+    ip.addParamValue('magnSigma2_prior',prior_unif('init'), @(x) isstruct(x) || isempty(x));
+    ip.addParamValue('lengthScale_prior',prior_unif('init'), @(x) isstruct(x) || isempty(x));
     ip.parse(do, varargin{:});
     do=ip.Results.do;
     gpcf=ip.Results.gpcf;
@@ -73,16 +73,9 @@ function gpcf = gpcf_sexp(do, varargin)
 
             % Initialize prior structure
             gpcf.p=[];
-            if isempty(lengthScale_prior)
-                gpcf.p.lengthScale=prior_unif('init');
-            else
-                gpcf.p.lengthScale=lengthScale_prior;
-            end
-            if isempty(magnSigma2_prior)
-                gpcf.p.magnSigma2=prior_unif('init');
-            else
-                gpcf.p.magnSigma2=magnSigma2_prior;
-            end
+            gpcf.p.lengthScale=lengthScale_prior;
+            gpcf.p.magnSigma2=magnSigma2_prior;
+
 
             %Initialize metric
             if ~isempty(metric)
