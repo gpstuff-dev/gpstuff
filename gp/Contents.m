b24626b132858b7046a2a768dc9b647a218d7966
Aki Vehtari <Aki.Vehtari@aalto.fi>
2010-10-18 10:50:18 +0000

    - some updates

--- gp/Contents.m
+++ gp/Contents.m
@@ -1,13 +1,13 @@
 % THE GP TOOLS (/in the GP/ folder):
 % 
 %  Gaussian process utilities:
-%   GPCOV     Evaluate covariance matrix between two input vectors. 
-%   GP_TRCOV  Evaluate training covariance matrix (gp_cov + noise covariance). 
-%   GP_TRVAR  Evaluate training variance vector. 
+%   GP_SET    Create and modify a Gaussian Process data structure. 
 %   GP_PAK    Combine GP hyper-parameters into one vector.
 %   GP_UNPAK  Set GP hyper-parameters from vector to structure
+%   GP_COV    Evaluate covariance matrix between two input vectors. 
+%   GP_TRCOV  Evaluate training covariance matrix (gp_cov + noise covariance). 
+%   GP_TRVAR  Evaluate training variance vector. 
 %   GP_RND    Random draws from the postrior Gaussian process
-%   GP_INIT   Create a Gaussian Process data structure. 
 %
 %  Covariance functions:
 %   GPCF_CONSTANT      Create a constant covariance function 
@@ -16,8 +16,6 @@
 %   GPCF_MATERN32      Create a squared exponential covariance function
 %   GPCF_MATERN52      Create a squared exponential covariance function
 %   GPCF_NEURALNETWORK Create a squared exponential covariance function
-%   GPCF_NOISE         Create a noise covariance function 
-%   GPCF_NOISET        Create a scale mixture noise covariance function (~Student-t) 
 %   GPCF_PERIODIC      Create a periodic covariance function
 %   GPCF_PPCS0         Create a piece wise polynomial (q=0) covariance function 
 %   GPCF_PPCS1         Create a piece wise polynomial (q=1) covariance function 
@@ -27,48 +25,61 @@
 %   GPCF_RQ            Create an rational quadratic covariance function 
 %   GPCF_SEXP          Create a squared exponential covariance function
 %
+%  Noise covariance functions:
+%   GPCF_NOISE         Create a noise covariance function 
+%   GPCF_NOISET        Create a scale mixture noise covariance function (~Student-t) 
+%
 %  Likelihood functions:
-%   LIKELIH_BINOMIAL   Create a binomial likelihood structure 
-%   LIKELIH_LOGIT      Create a Logit likelihood structure 
-%   LIKELIH_NEGBIN     Create a Negbin likelihood structure 
-%   LIKELIH_POISSON    Create a Poisson likelihood structure 
-%   LIKELIHOOD_PROBIT  Create a Probit likelihood structure 
-%   LIKELIH_T          Create a Student-t likelihood structure 
+%   LIK_GAUSSIAN   Return string 'gaussian'
+%   LIK_BINOMIAL   Create a binomial likelihood structure 
+%   LIK_LOGIT      Create a Logit likelihood structure 
+%   LIK_NEGBIN     Create a Negbin likelihood structure 
+%   LIK_POISSON    Create a Poisson likelihood structure 
+%   LIK_PROBIT     Create a Probit likelihood structure 
+%   LIK_T          Create a Student-t likelihood structure 
 %
 % Inference utilities:
-%   GP_E          Evaluate energy function (un-normalized marginal log posterior) 
-%                 in case of Gaussian observation model
+%   GP_E          Evaluate energy function (un-normalized negative marginal 
+%                 log posterior) 
 %   GP_G          Evaluate gradient of energy (GP_E) for Gaussian Process
+%   GP_EG         Evaluate both GP_E and GP_G. Useful in optimisation.
 %   GP_PRED       Make predictions with Gaussian process 
-%   GPEP_E        Conduct Expectation propagation and return marginal 
+%   GPEP_E        Conduct Expectation propagation and return negative marginal 
 %                 log posterior estimate
-%   GPEP_G        Evaluate gradient of EP's marginal log posterior estimate (GPEP_E)
+%   GPEP_G        Evaluate gradient of EP's negative marginal log posterior 
+%                 estimate
 %   EP_PRED       Predictions with Gaussian Process EP approximation
-%   GPLA_E        Construct Laplace approximation and return marginal 
+%   GPLA_E        Construct Laplace approximation and return negative marginal 
 %                 log posterior estimate
 %   GPLA_G        Evaluate gradient of Laplace approximation's marginal 
-%                 log posterior estimate (GPLA_E)
+%                 log posterior estimate
 %   LA_PRED       Predictions with Gaussian Process Laplace approximation
 %   GP_MC         Markov chain sampling for Gaussian process models
 %   MC_PRED       Predictions with Gaussian Process MCMC approximation.
-%   GP_IA         Integration approximation with grid, Monte Carlo or CCD integration
+%   MC_PREDS      Conditional predictions with Gaussian Process MCMC
+%                 approximation.
+%   GP_IA         Integration approximation with grid, Monte Carlo or
+%                 CCD integration
 %   IA_PRED       Prediction with Gaussian Process GP_IA solution.
-%    LGCP         Log Gaussian Cox Process intensity estimate for 1D and 2D data
+%   LGCP          Log Gaussian Cox Process intensity estimate for 1D and 
+%                 2D data
 %
 %  Model assesment and comparison:
 %   GP_DIC        The DIC statistics and efective number of parameters in a GP model
 %   GP_KFCV       K-fold cross validation for a GP model
-%   GP_LOOE       Evaluate the leave one out predictive density in case of
+%   GP_LOOE       Evaluate the leave-one-out predictive density in case of
 %                 Gaussian observation model
-%   GP_LOOE       Evaluate the gradient of the leave one out predictive 
+%   GP_LOOG       Evaluate the gradient of the leave-one-out predictive 
 %                 density (GP_LOOE) in case of Gaussian observation model 
+%   GP_EPLOOE     Evaluate the leave-one-out predictive density in case of
+%                 non-Gaussian observation model and EP
+%   EP_LOOPRED    Leave-one-out-predictions with Gaussian Process EP approximation
 %   GP_PEFF       The efective number of parameters in GP model with focus 
 %                 on latent variables.
 %
 %  Metrics:
-%   METRIC_DISTANCEMATRIX  An Euclidean distance for Gaussian process models. 
-%   METRIC_EUCLIDEAN       An Euclidean distance for Gaussian process models.
-%   METRIC_IBS_GXE         An Euclidean distance for Gaussian process models.
+%   METRIC_EUCLIDEAN   An Euclidean distance for Gaussian process models.
+%   METRIC_DISTMATRIX  Use pre-computed distance matrix
 %  
 %  Misc:
 %    LDLROWMODIFY  Function to modify the sparse cholesky factorization 
