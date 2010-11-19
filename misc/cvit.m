d023c6bfa5aa27689bbfd57dd9cc2165b3ad1dd1
Aki Vehtari <Aki.Vehtari@aalto.fi>
2010-11-19 14:50:55 +0000

    - cvitr.m removed - cvit help text fix

--- misc/cvit.m
+++ misc/cvit.m
@@ -1,17 +1,17 @@
 function [itr, itst] = cvit(n, k, rsubstream)
-% CVIT - Create itr and itst indeces for k-fold-cv
+%CVIT  Create itr and itst indeces for k-fold-cv
 %
-%    Description
-%     [ITR,ITST]=CVITR(N,K) returns 1xK cell arrays ITR and ITST holding 
-%      cross-validation indeces for train and test sets respectively. 
-%      K-fold division is balanced with all sets having floor(N/K) or 
-%      ceil(N/K) elements.
+%  Description
+%    [ITR,ITST]=CVIT(N,K) returns 1xK cell arrays ITR and ITST
+%    holding cross-validation indeces for train and test sets
+%    respectively. K-fold division is balanced with all sets having
+%    floor(N/K) or ceil(N/K) elements.
 %
-%     [ITR,ITST]=CVITR(N,K,RS) with integer RS>0 also makes random 
-%      permutation, using substream RS. This way different permutations 
-%      can be produced with different RS values, but same permutation is 
-%      obtained when called again with same RS. Function restores the 
-%      previous random stream before exiting.
+%    [ITR,ITST]=CVIT(N,K,RS) with integer RS>0 also makes random
+%    permutation, using substream RS. This way different
+%    permutations can be produced with different RS values, but
+%    same permutation is obtained when called again with same RS. 
+%    Function restores the previous random stream before exiting.
 %
 
 % Copyright (c) 2010 Aki Vehtari
