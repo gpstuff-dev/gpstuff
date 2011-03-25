ce39a32a27b4fa4dda2f5a2bf7d122313de24780
Aki Vehtari <Aki.Vehtari@aalto.fi>
2011-03-25 15:01:32 +0000

    - An experimental feature for gp_optim   %      energy - 'e' to minimize the marginal posterior energy (default) or   %               'looe' to minimize the leave-one-out energy - If lik is gaussian-type uses gradients - If lik is non-gaussian uses gpep_looe but not no gradients

---
+++ gp/gp_looeg.m
@@ -0,0 +1,50 @@
+function [e, g] = gp_looeg(w, gp, x, y, varargin)
+%GP_LOOEG  Evaluate the mean negative log leave-one-out predictive density
+%
+%  Description
+%    [E, G] = GP_LOOEG(W, GP, X, Y, OPTIONS) takes a Gaussian
+%    process structure GP together with a matrix X of input vectors
+%    and a matrix Y of targets, and evaluates the energy function E
+%    and its gradient G (only with Gaussian likelihood). Each row
+%    of X corresponds to one input vector and each row of Y
+%    corresponds to one target vector.
+%
+%    The energy is the mean negative log
+%    leave-one-out predictive density
+%       LOOE  = - 1/n sum log p(Y_i | X, Y_{\i}, th)
+%    where th represents the parameters (lengthScale,
+%    magnSigma2...), X is inputs and Y is observations.
+%
+%    For non-Gaussian models EP leave-one-out is used for energy
+%    (see GPEP_LOOE), but no gradients are yet implemented, and
+%    thus gradient-free optimization function has to be used.
+%
+%    OPTIONS is optional parameter-value pair
+%      z - optional observed quantity in triplet (x_i,y_i,z_i)
+%          Some likelihoods may use this. For example, in case of
+%          Poisson likelihood we have z_i=E_i, that is, expected
+%          value for ith case.
+%
+%  See also
+%    GP_LOOE, GP_LOOG, GPEP_LOOE
+%
+
+% Copyright (c) 2010 Aki Vehtari
+  
+% Single function for some optimization routines, no need for mydeal...
+if isfield(gp.lik.fh,'trcov')
+  % Gaussian likelihood
+  e=gp_looe(w, gp, x, y);
+  if nargout>1
+    g=gp_loog(w, gp, x, y);
+  end
+else
+  % non-Gaussian likelihood
+  if ~isequal(gp.latent_method,'EP')
+    gp=gp_set(gp,'latent_method','EP');
+  end
+  e=gpep_looe(w, gp, x, y, varargin{:});
+  if nargout>1
+    error('EP leave-one-out does not have gradients yet, use gradient-free optimization.')
+  end
+end
--- gp/gp_optim.m
+++ gp/gp_optim.m
@@ -25,6 +25,14 @@ function [gp, varargout] = gp_optim(gp, x, y, varargin)
 %    GP_SET, GP_E, GP_G, GP_EG, FMINSCG, FMINLBFGS, OPTIMSET, DEMO_REGRESSION*
 %
 
+%  Experimental
+%      energy - 'e' to minimize the marginal posterior energy (default) or
+%               'looe' to minimize the leave-one-out energy
+%
+%  See also
+%    GP_SET, GP_E, GP_G, GP_EG, GP_LOOE, GP_LOOG, GP_LOOEG, GPEP_LOOE, 
+%    FMINSCG, FMINLBFGS, OPTIMSET, DEMO_REGRESSION*
+
 % Copyright (c) 2010 Aki Vehtari
 
 % This software is distributed under the GNU General Public
@@ -39,32 +47,49 @@ ip.addRequired('y', @(x) ~isempty(x) && isreal(x) && all(isfinite(x(:))))
 ip.addParamValue('z', [], @(x) isreal(x) && all(isfinite(x(:))))
 ip.addParamValue('optimf', @fminscg, @(x) isa(x,'function_handle'))
 ip.addParamValue('opt', [], @isstruct)
+ip.addParamValue('energy', 'e', @(x) ismember(x,{'e', 'looe'}))
 ip.parse(gp, x, y, varargin{:});
 z=ip.Results.z;
 optimf=ip.Results.optimf;
 opt=ip.Results.opt;
+energy=ip.Results.energy;
 
-optdefault=struct('GradObj','on','LargeScale','off');
+switch energy
+  case 'e'
+    fh_eg=@gp_eg;
+    optdefault=struct('GradObj','on','LargeScale','off');
+  case 'looe'
+    fh_eg=@gp_looeg;
+    if isfield(gp.lik.fh,'trcov')
+      optdefault=struct('GradObj','on','LargeScale','off');
+    else
+      % EP-LOO does not have yet gradients
+      optdefault=struct('GradObj','off','LargeScale','off');
+      if ismember('optimf',ip.UsingDefaults)
+        optimf=@fminunc;
+      end
+    end
+end
 opt=optimset(optdefault,opt);
 w=gp_pak(gp);
 switch nargout
   case 6
-    [w,fval,exitflag,output,grad,hessian] = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    [w,fval,exitflag,output,grad,hessian] = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={fval,exitflag,output,grad,hessian};
   case 5
-    [w,fval,exitflag,output,grad] = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    [w,fval,exitflag,output,grad] = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={fval,exitflag,output,grad};
   case 4
-    [w,fval,exitflag,output] = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    [w,fval,exitflag,output] = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={fval,exitflag,output};
   case 3
-    [w,fval,exitflag] = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    [w,fval,exitflag] = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={fval,exitflag};
   case 2
-    [w,fval] = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    [w,fval] = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={fval};
   case 1
-    w = optimf(@(ww) gp_eg(ww, gp, x, y, 'z', z), w, opt);
+    w = optimf(@(ww) fh_eg(ww, gp, x, y, 'z', z), w, opt);
     varargout{:}={};
 end
 gp=gp_unpak(gp,w);
