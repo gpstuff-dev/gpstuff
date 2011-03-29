3a72f08383020128a789b3a5f8b2662f156a6e34
Aki Vehtari <Aki.Vehtari@aalto.fi>
2011-03-29 16:16:26 +0000

    - Accept optional parameters m,n

--- mc/linuxCsource/resampstr.c
+++ mc/linuxCsource/resampstr.c
@@ -2,7 +2,7 @@
  *
  * (C) 2003 Aki Vehtari
  *
- * Last modified: 2004-10-12 15:23:58 EEST
+ * Last modified: 2011-03-29 19:13:44 EEST
  *
  */
 
@@ -15,53 +15,71 @@ void mexFunction(int nlhs, mxArray *plhs[],
   if (nlhs > 1 ) 
     mexErrMsgTxt( "Too many output arguments." );
   
-  if (nrhs != 1)
+  if (nrhs < 1 || nrhs > 3)
     mexErrMsgTxt( "Wrong number of input arguments." );
-
+  
   {
     mxArray *MN[2], *R;
     double *p, *q, *r, *s, sum=0, b, c=0; 
     const int *dims;
-    int i, n, a, k=0;
+    int i, m, n, mn, pn, a, k=0;
 
     dims = mxGetDimensions(prhs[0]);
-    n = dims[0]*dims[1];            /* number of weights */
-    p = mxGetPr(prhs[0]);           /* pointer to unormalized weights */
-    q = mxMalloc(n*sizeof(double)); /* pointer to normalized and scaled weights */
+    pn = dims[0]*dims[1];            /* number of weights */
+    p = mxGetPr(prhs[0]);            /* pointer to unormalized weights */
+    q = mxMalloc(pn*sizeof(double)); /* pointer to normalized and scaled weights */
 
-    /* 'n' uniform random numbers, 'MN' is needed only temporarily */
-    MN[0]=mxCreateDoubleScalar(dims[0]);
-    MN[1]=mxCreateDoubleScalar(dims[1]);
+    if (nrhs==1) {
+      m = dims[0];
+      n = dims[1];
+    }
+    else { /* nrhs > 1 */
+      dims = mxGetDimensions(prhs[1]);
+      if (dims[0]>1 || dims[1]>1)
+	mexErrMsgTxt("M must be a scalar.");
+      m = mxGetScalar(prhs[1]);
+      if (nrhs==2) {
+	n = m;
+      }	else {
+	dims = mxGetDimensions(prhs[2]);
+	if (dims[0]>1 || dims[1]>1)
+	  mexErrMsgTxt("M must be a scalar.");
+	n = mxGetScalar(prhs[2]);
+      }
+    }
+    mn = m*n;
+
+    MN[0]=mxCreateDoubleScalar(m);
+    MN[1]=mxCreateDoubleScalar(n);
     mexCallMATLAB(1,&R,2,MN,"rand");
     mxDestroyArray(MN[0]);
     mxDestroyArray(MN[1]);
-    r = mxGetPr(R);                 /* pointer to uniform random numbers */
+    r = mxGetPr(R);           /* pointer to uniform random numbers */
 
     /* allocate matrix for return value */
-    plhs[0]=mxCreateDoubleMatrix(dims[0],dims[1],mxREAL);
+    plhs[0]=mxCreateDoubleMatrix(m,n,mxREAL);
     s = mxGetPr(plhs[0]);     /* pointer to samples */
 
-    for (i = 0; i < n; i++)
-      sum+=p[i];              /* compute sum for normalization */
-    for (i = 0; i < n; i++) {
-      q[i]=p[i]/sum*n;        /* normalize and scale weight */
-      c+=q[i];                /* cumulate weights */
-      if (c>=1.0) {           /* if cumulative weight over 1 */
+    for (i = 0; i < pn; i++)
+      sum+=p[i];              /* compute the sum for normalization */
+    for (i = 0; i < pn; i++) {
+      q[i]=p[i]/sum*mn;       /* normalize and scale the weight */
+      c+=q[i];                /* the cumulative weight */
+      if (c>=1.0) {           /* if the cumulative weight over 1 */
 	a=(int)(c);           /* integer part of the cumulative weight */
-	c-=a;                 /* subract integer part from the cum weight */
+	c-=a;                 /* subract integer part from the cumulative weight */
 	k+=a;                 
 	for (b=i+1.0;a>0;a--)	  
-	  *s++=b;             /* fill vector 'a' times with sample 'b' */
+	  *s++=b;             /* fill the return matrix 'a' times with sample 'b' */
       }
-      if (k<n && c>=r[k]) {   /* if cumulative larger than a random number...*/
+      if (k<mn && c>=r[k]) {  /* if the cumulative larger than a random number...*/
 	*s++=i+1.0;           /* .. add sample... */
-	c-=1.0;               /* ...and subract one from the cumulative */
+	c-=1.0;               /* ...and subtract one from the cumulative */
 	k+=1;                 
       }
     }
-
     mxDestroyArray(R);        /* no need for this anymore */
-
+    
   }
 
   return;
