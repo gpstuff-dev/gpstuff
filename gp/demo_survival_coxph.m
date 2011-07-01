2d16b0deea0865574abb5447c329bec160883bf8
Jaakko Riihimäki <Jaakko.Riihimaki@aalto.fi>
2011-07-01 14:17:00 +0000

    -Added left-truncated right-censored example where baseline hazard is a function of age instead of follow-up time

--- gp/demo_survival_coxph.m
+++ gp/demo_survival_coxph.m
@@ -12,6 +12,17 @@
 %    Gaussian approximation for p(f| th, data), where th is the
 %    maximum a posterior (MAP) estimate for the parameters.
 %    
+%    The censoring indicator ye is
+%    
+%      ye = 0 for uncensored event
+%      ye = 1 for right censored event.
+%
+%    If survival times y for n observation are given as nx2 matrix with
+%    entry times into follow-up in the first column and exit times from
+%    follow-up in the second column, left truncated right censored
+%    modelling is possible, for instance, in cases where age is wanted to
+%    be set as a baseline hazard.  
+%
 %    Example data set is leukemia survival data in Northwest England
 %    presented in (Henderson, R., Shimakura, S., and Gorst, D. (2002).
 %    Modeling spatial variation in leukemia survival data. Journal of the
@@ -88,7 +99,7 @@ gp = gp_set(gp, 'latent_method', 'Laplace');
 %gpla_nd_e(gp_pak(gp),gp,x,y,'z',ye)
 %gpla_nd_g(gp_pak(gp),gp,x,y,'z',ye);
 
-opt=optimset('TolFun',1e-2,'TolX',1e-2,'Display','iter','Derivativecheck','on');
+opt=optimset('TolFun',1e-2,'TolX',1e-2,'Display','iter','Derivativecheck','off');
 gp=gp_optim(gp,x,y,'z',ye,'opt',opt);
 
 % Make prediction
@@ -109,7 +120,7 @@ figure, hold on, set(gcf, 'color', 'w'),
 plot(gp.lik.xtime, Ef1(1:ntime),'k', 'linewidth', 3)
 plot(gp.lik.xtime, Ef1(1:ntime)+1.96*sqrt(Varf1(1:ntime)),'--k', 'linewidth', 2)
 plot(gp.lik.xtime, Ef1(1:ntime)-1.96*sqrt(Varf1(1:ntime)),'--k', 'linewidth', 2)
-title('log-baseline hazard')
+title('log-baseline hazard (follow-up time)')
 xlabel('time')
 
 % Compute posterior mean and 95% credible intervals of latent function as a
@@ -124,7 +135,43 @@ plot(xt01(:,1), Ef1(ntime+1:end)-1.96*sqrt(Varf1(ntime+1:end)), '--', 'color', c
 plot(xt02(:,1), Ef2(ntime+1:end), 'color', col2, 'linewidth', 3)
 plot(xt02(:,1), Ef2(ntime+1:end)+1.96*sqrt(Varf2(ntime+1:end)), '--', 'color', col2, 'linewidth', 2)
 plot(xt02(:,1), Ef2(ntime+1:end)-1.96*sqrt(Varf2(ntime+1:end)), '--', 'color', col2, 'linewidth', 2)
-
 xlabel('age')
 title('effect of age for both sexes')
 
+
+%- Age as baseline hazard
+
+% Age in the beginning and in the end of follow-up
+y=[x0(:,1) x0(:,1)+leukemiadata(:,1)/365];
+y2=y;
+% normalise ages
+y2=y2-min(y2(:)); y2=y2./max(y2(:));
+
+x2=x;
+x2(:,1)=[];
+
+% covariance for proportional part
+gpcf = gpcf_sexp('lengthScale', ones(1,size(x2,2)), 'magnSigma2', .5, 'lengthScale_prior', pl, 'magnSigma2_prior', pm);
+
+% NOTE! if Multible covariance functions per latent is used, define
+% gp.comp_cf as follows:
+% gp.comp_cf = {[1 2] [5 6]}
+% where [1 2] are for hazard function, and [5 6] for proportional part
+gp = gp_set('lik', lik, 'cf', {gpcfh gpcf}, 'jitterSigma2', 1e-6);
+gp.comp_cf = {[1] [2]};
+
+% Set the approximate inference method to Laplace
+gp = gp_set(gp, 'latent_method', 'Laplace');
+
+opt=optimset('TolFun',1e-2,'TolX',1e-2,'Display','iter','Derivativecheck','off');
+gp=gp_optim(gp,x2,y2,'z',ye,'opt',opt);
+
+[Ef1, Varf1] = gp_pred(gp, x2, y2, x2, 'z', ye);
+xtmpl=linspace(min(y(:)),max(y(:)),50);
+
+figure, hold on, set(gcf, 'color', 'w'),
+plot(xtmpl, Ef1(1:ntime),'k', 'linewidth', 3)
+plot(xtmpl, Ef1(1:ntime)+1.96*sqrt(Varf1(1:ntime)),'--k', 'linewidth', 2)
+plot(xtmpl, Ef1(1:ntime)-1.96*sqrt(Varf1(1:ntime)),'--k', 'linewidth', 2)
+title('log-baseline hazard (age)')
+xlabel('age')
