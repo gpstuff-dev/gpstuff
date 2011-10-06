12a71bbaa9e3c68e9d2ab0fb0d73f5c9e1d20d66
Aki Vehtari <Aki.Vehtari@aalto.fi>
2011-10-06 13:17:30 +0000

    - moved randpick to mc - updated Contents

--- misc/Contents.m
+++ misc/Contents.m
@@ -3,14 +3,14 @@
 %    CVIT         - Create itr and itst indeces for k-fold-cv
 %    CVITR        - Create itr and itst indeces for k-fold-cv with ranndom 
 %                   permutation
+%    NORMDATA  Normalize input to zero mean and unit variance
+%    DENORMDATA  De-normalize normalized data
 %    MAPCOLOR     - returns a colormap ranging from blue through gray
 %                   to red
 %    MAPCOLOR2    - Create a blue-gray-red colormap.
 %    M2KML        - Converts GP prediction results to a KML file
-%    QUAD_MOMENTS - Calculate the 0th, 1st and 2nd moment of a given (unnormalized) 
-%                   probability distribution
-%    RANDPICK     - Pick element from x randomly
-%                   If x is matrix, pick row from x randomly.
+%    LOGITINV     - Inverse of the logit transformation
+%    LOGIT        - Logit transformation
 %    STR2FUN      - Compatibility wrapper to str2func
 %    SET_PIC      - Set the inducing inputs and blocks for two dimensional input data
 %    WMEAN        - weighted mean
