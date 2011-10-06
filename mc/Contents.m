15e79a1e3c89521dacb536dbe60dd6b93f76536f
Aki Vehtari <Aki.Vehtari@aalto.fi>
2011-10-06 13:22:59 +0000

    - moved softmax2 to misc - updated Contents

--- mc/Contents.m
+++ mc/Contents.m
@@ -17,4 +17,4 @@
 %    SLS_OPT       - Default options for Slice Sampling
 %    SLS1MM        - 1-dimensional fast minmax Slice Sampling
 %    SLS1MM_OPT    - Default options for SLS1MM_OPT
-%    SOFTMAX2      - Softmax transfer function
+%
