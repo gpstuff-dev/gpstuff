d0dfca87b974bb2a8ac505b5fd386a9ce7bf14a1
Ville Tolvanen <Ville.Tolvanen@aalto.fi>
2011-10-20 13:02:46 +0000

    updated tests

--- xunit/test_regression_additive1.m
+++ xunit/test_regression_additive1.m
@@ -1,4 +1,4 @@
-function test_suite = test_regression2
+function test_suite = test_regression_additive1
 initTestSuite;
 
 % Set random number stream so that test failing isn't because randomness.
@@ -7,14 +7,14 @@ initTestSuite;
 function testDemo
     stream0 = RandStream('mt19937ar','Seed',0);
     RandStream.setDefaultStream(stream0);
-    disp('Running: demo_regression2')
-    demo_regression2
-    path = which('test_regression2.m');
-    path = strrep(path,'test_regression2.m', 'testValues');
+    disp('Running: demo_regression_additive1')
+    demo_regression_additive1
+    path = which('test_regression_additive1.m');
+    path = strrep(path,'test_regression_additive1.m', 'testValues');
     if ~(exist(path, 'dir') == 7)
         mkdir(path)
     end
-    path = strcat(path, '/testRegression2');     
+    path = strcat(path, '/testRegression_additive1');     
     save(path, 'Eft_fic', 'Varft_fic', 'Eft_pic', 'Varft_pic', ...
          'Eft_csfic', 'Varft_csfic');
     drawnow;clear;close all
@@ -22,20 +22,20 @@ function testDemo
 % Compare test values to real values.
 
 function testPredictionsFIC
-    values.real = load('realValuesRegression2.mat', 'Eft_fic', 'Varft_fic');
-    values.test = load(strrep(which('test_regression2.m'), 'test_regression2.m', 'testValues/testRegression2.mat'), 'Eft_fic', 'Varft_fic');
+    values.real = load('realValuesRegression_additive1.mat', 'Eft_fic', 'Varft_fic');
+    values.test = load(strrep(which('test_regression_additive1.m'), 'test_regression_additive1.m', 'testValues/testRegression_additive1.mat'), 'Eft_fic', 'Varft_fic');
     assertElementsAlmostEqual(mean(values.real.Eft_fic), mean(values.test.Eft_fic), 'relative', 0.05);
 	assertElementsAlmostEqual(mean(values.real.Varft_fic), mean(values.test.Varft_fic), 'relative', 0.05);
     
 function testPredictionsPIC 
-    values.real = load('realValuesRegression2.mat', 'Eft_pic', 'Varft_pic');
-    values.test = load(strrep(which('test_regression2.m'), 'test_regression2.m', 'testValues/testRegression2.mat'), 'Eft_pic', 'Varft_pic');
+    values.real = load('realValuesRegression_additive1.mat', 'Eft_pic', 'Varft_pic');
+    values.test = load(strrep(which('test_regression_additive1.m'), 'test_regression_additive1.m', 'testValues/testRegression_additive1.mat'), 'Eft_pic', 'Varft_pic');
     assertElementsAlmostEqual(mean(values.real.Eft_pic), mean(values.test.Eft_pic), 'relative', 0.05);
 	assertElementsAlmostEqual(mean(values.real.Varft_pic), mean(values.test.Varft_pic), 'relative', 0.05);
     
 
 function testPredictionsSparse
-    values.real = load('realValuesRegression2.mat', 'Eft_csfic', 'Varft_csfic');
-    values.test = load(strrep(which('test_regression2.m'), 'test_regression2.m', 'testValues/testRegression2.mat'), 'Eft_csfic', 'Varft_csfic');
+    values.real = load('realValuesRegression_additive1.mat', 'Eft_csfic', 'Varft_csfic');
+    values.test = load(strrep(which('test_regression_additive1.m'), 'test_regression_additive1.m', 'testValues/testRegression_additive1.mat'), 'Eft_csfic', 'Varft_csfic');
     assertElementsAlmostEqual(mean(values.real.Eft_csfic), mean(values.test.Eft_csfic), 'relative', 0.05);
 	assertElementsAlmostEqual(mean(values.real.Varft_csfic), mean(values.test.Varft_csfic), 'relative', 0.05);
--- xunit/test_regression_additive2.m
+++ xunit/test_regression_additive2.m
@@ -1,4 +1,4 @@
-function test_suite = test_regression_additive
+function test_suite = test_regression_additive2
 initTestSuite;
 
 
@@ -8,20 +8,20 @@ initTestSuite;
 function testDemo
     stream0 = RandStream('mt19937ar','Seed',0);
     RandStream.setDefaultStream(stream0);
-    disp('Running: demo_regression_additive')
-    demo_regression_additive
-    path = which('test_regression_additive.m');
-    path = strrep(path,'test_regression_additive.m', 'testValues');
+    disp('Running: demo_regression_additive2')
+    demo_regression_additive2
+    path = which('test_regression_additive2.m');
+    path = strrep(path,'test_regression_additive2.m', 'testValues');
     if ~(exist(path, 'dir') == 7)
         mkdir(path)
     end
-    path = strcat(path, '/testRegression_additive');     
+    path = strcat(path, '/testRegression_additive2');     
     save(path, 'Eft_map');
     drawnow;clear;close all
     
 % Compare test values to real values.    
 
 function testNeuralNetworkCFPrediction
-    values.real = load('realValuesRegression_additive.mat','Eft_map');
-    values.test = load(strrep(which('test_regression_additive.m'), 'test_regression_additive.m', 'testValues/testRegression_additive.mat'),'Eft_map');
+    values.real = load('realValuesRegression_additive2.mat','Eft_map');
+    values.test = load(strrep(which('test_regression_additive2.m'), 'test_regression_additive2.m', 'testValues/testRegression_additive2.mat'),'Eft_map');
     assertElementsAlmostEqual(mean(values.real.Eft_map), mean(values.test.Eft_map), 'relative', 0.05);
