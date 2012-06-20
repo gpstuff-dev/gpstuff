7d8c005cb557a2fe5425723cf7d4cf74670f9bba
Aki Vehtari <Aki.Vehtari@aalto.fi>
2012-06-20 12:04:32 +0000

    - updated contents

--- misc/Contents.m
+++ misc/Contents.m
@@ -1,19 +1,20 @@
 % MISCELLANEOUS FUNCTIONS (in the misc-folder)
 %
+%    ADDLOGS      - Add numbers represented by their logarithms.
 %    BINSGEQ      - Binary search of sorted vector
 %    CVIT         - Create itr and itst indeces for k-fold-cv
-%    CVITR        - Create itr and itst indeces for k-fold-cv with ranndom 
-%                   permutation
-%    NORMDATA  Normalize input to zero mean and unit variance
-%    DENORMDATA  De-normalize normalized data
-%    MAPCOLOR     - returns a colormap ranging from blue through gray
-%                   to red
-%    MAPCOLOR2    - Create a blue-gray-red colormap.
-%    M2KML        - Converts GP prediction results to a KML file
-%    LOGITINV     - Inverse of the logit transformation
+%    CVITR        - Create itr and itst indeces for k-fold-cv with ranndom permutation
+%    DENORMDATA   - De-normalize normalized data
 %    LOGIT        - Logit transformation
-%    SOFTMAX2      - Softmax transfer function
-%    STR2FUN      - Compatibility wrapper to str2func
+%    LOGITINV     - Inverse of the logit transformation
+%    M2KML        - Converts GP prediction results to a KML file
+%    MAPCOLOR     - Returns a colormap ranging from blue through gray to red
+%    MAPCOLOR2    - Create a blue-gray-red colormap.
+%    NORMDATA     - Normalize input to zero mean and unit variance
 %    SET_PIC      - Set the inducing inputs and blocks for two dimensional input data
-%    WMEAN        - weighted mean
+%    SOFTMAX2     - Softmax transfer function
+%    STR2FUN      - Compatibility wrapper to str2func
+%    SUMLOGS      - Sum of vector where numbers are represented by their logarithms.
+%    VIOLINPLOT   - Plot a violinplot
+%    WMEAN        - Weighted mean
 %
