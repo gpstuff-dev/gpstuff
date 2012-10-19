c2d4cc8249be01c78dbe9f067731acf8ebfd63ee
Ville Tolvanen <Ville.Tolvanen@aalto.fi>
2012-10-19 12:30:28 +0000

    updated tests/values todo: update modelassesment2 values

--- xunit/test_modelassesment1.m
+++ xunit/test_modelassesment1.m
@@ -24,7 +24,7 @@ initTestSuite;
     end
     path = strcat(path, '/testModelAssesment1');
     save(path, 'DIC', 'DIC2', 'DIC_latent', ...
-      'p_eff', 'p_eff2', 'p_eff_latent', 'p_eff_latent2', 'mlpd_cv', 'mrmse_cv');
+      'p_eff', 'p_eff2', 'p_eff_latent', 'p_eff_latent2', 'mlpd_cv', 'rmse_cv', 'mlpd_loo', 'rmse_loo');
     
     % Set back initial random stream
     setrandstream(prevstream);
@@ -33,44 +33,58 @@ initTestSuite;
   % Compare test values to real values.
 
   function testDICParameters
-    values.real = load('realValuesModelAssesment1.mat','DIC');
+    values.real = load('realValuesModelAssesment1.mat','DIC'); 
+    values.real.DIC(isnan(values.real.DIC)) = 0;
     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'DIC');
+    values.test.DIC(isnan(values.test.DIC)) = 0;
     assertVectorsAlmostEqual(values.real.DIC, values.test.DIC, 'relative', 0.20);
     
         
   function testDICAll
     values.real = load('realValuesModelAssesment1.mat','DIC2');
+    values.real.DIC2(isnan(values.real.DIC2)) = 0;
     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'DIC2');
+    values.test.DIC2(isnan(values.test.DIC2)) = 0;
     assertVectorsAlmostEqual(values.real.DIC2, values.test.DIC2, 'relative', 0.20);
     
         
   function testDICLatent
     values.real = load('realValuesModelAssesment1.mat','DIC_latent');
+    values.real.DIC_latent(isnan(values.real.DIC_latent)) = 0;
     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'DIC_latent');
+    values.test.DIC_latent(isnan(values.test.DIC_latent)) = 0;
     assertVectorsAlmostEqual(values.real.DIC_latent, values.test.DIC_latent, 'relative', 0.20);
     
         
    function testPeffLatentMarginalized
      values.real = load('realValuesModelAssesment1.mat','p_eff');
+     values.real.p_eff(isnan(values.real.p_eff)) = 0;
      values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'p_eff');
+     values.test.p_eff(isnan(values.test.p_eff)) = 0;
      assertVectorsAlmostEqual(values.real.p_eff, values.test.p_eff, 'relative', 0.20);
      
 
    function testPeffAll
      values.real = load('realValuesModelAssesment1.mat','p_eff2');
+     values.real.p_eff2(isnan(values.real.p_eff2)) = 0;
      values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'p_eff2');
+     values.test.p_eff2(isnan(values.test.p_eff2)) = 0;
      assertVectorsAlmostEqual(values.real.p_eff2, values.test.p_eff2, 'relative', 0.20);
      
         
    function testPeffLatent
      values.real = load('realValuesModelAssesment1.mat','p_eff_latent');
+     values.real.p_eff_latent(isnan(values.real.p_eff_latent)) = 0;
      values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'p_eff_latent');
+     values.test.p_eff_latent(isnan(values.test.p_eff_latent)) = 0;
      assertVectorsAlmostEqual(values.real.p_eff_latent, values.test.p_eff_latent, 'relative', 0.20);
      
         
    function testPeffLatent2
      values.real = load('realValuesModelAssesment1.mat','p_eff_latent2');
+     values.real.p_eff_latent2(isnan(values.real.p_eff_latent2)) = 0;
      values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'),'p_eff_latent2');
+     values.test.p_eff_latent2(isnan(values.test.p_eff_latent2)) = 0;
      assertVectorsAlmostEqual(values.real.p_eff_latent2, values.test.p_eff_latent2, 'relative', 0.20);
      
      
@@ -81,7 +95,18 @@ initTestSuite;
      
         
    function testMeanSquaredError10foldCV
-     values.real = load('realValuesModelAssesment1.mat', 'mrmse_cv');
-     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'), 'mrmse_cv');
+     values.real = load('realValuesModelAssesment1.mat', 'rmse_cv');
+     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'), 'rmse_cv');
      assertVectorsAlmostEqual(values.real.mrmse_cv, values.test.mrmse_cv, 'relative', 0.20);
+                 
+   function testLogPredDensityLOOPRED
+     values.real = load('realValuesModelAssesment1.mat', 'mlpd_loo');
+     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'), 'mlpd_loo');
+     assertVectorsAlmostEqual(values.real.mlpd_loo, values.test.mlpd_loo, 'relative', 0.20);
+     
+        
+   function testMeanSquaredErrorLOOPRED
+     values.real = load('realValuesModelAssesment1.mat', 'rmse_loo');
+     values.test = load(strrep(which('test_modelassesment1.m'), 'test_modelassesment1.m', 'testValues/testModelAssesment1.mat'), 'rmse_loo');
+     assertVectorsAlmostEqual(values.real.rmse_loo, values.test.rmse_loo, 'relative', 0.20);
                  
--- xunit/test_modelassesment2.m
+++ xunit/test_modelassesment2.m
@@ -24,7 +24,7 @@ initTestSuite;
     end
     path = strcat(path, '/testModelAssesment2');
     save(path, 'DIC', 'DIC2', 'DIC_latent', 'p_eff', ...
-      'p_eff2', 'p_eff_latent', 'p_eff_latent2', 'mlpd_cv');
+      'p_eff2', 'p_eff_latent', 'p_eff_latent2', 'mlpd_cv', 'mlpd_loo');
     
     % Set back initial random stream
     setrandstream(prevstream);
@@ -34,37 +34,49 @@ initTestSuite;
 
   function testDICParameters
     values.real = load('realValuesModelAssesment2.mat', 'DIC');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'DIC');
+    values.real.DIC(isnan(values.real.DIC)) = 0;
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'DIC');
+    values.test.DIC(isnan(values.test.DIC)) = 0;    
     assertVectorsAlmostEqual(values.real.DIC, values.test.DIC, 'relative', 0.05);
 
         
   function testDICAll
     values.real = load('realValuesModelAssesment2.mat', 'DIC2');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'DIC2');
+    values.real.DIC2(isnan(values.real.DIC2)) = 0;
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'DIC2');
+    values.test.DIC2(isnan(values.test.DIC2)) = 0;
     assertVectorsAlmostEqual(values.real.DIC2, values.test.DIC2, 'relative', 0.05);
 
         
   function testDICLatent
     values.real = load('realValuesModelAssesment2.mat', 'DIC_latent');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'DIC_latent');
+    values.real.DIC_latent(isnan(values.real.DIC_latent)) = 0;
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'DIC_latent');
+    values.test.DIC_latent(isnan(values.test.DIC_latent)) = 0;
     assertVectorsAlmostEqual(values.real.DIC_latent, values.test.DIC_latent, 'relative', 0.05);
 
         
   function testPeffLatentsMarginalized
     values.real = load('realValuesModelAssesment2.mat', 'p_eff');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'p_eff');
+    values.real.p_eff(isnan(values.real.p_eff)) = 0;
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'p_eff');
+    values.test.p_eff(isnan(values.test.p_eff)) = 0;
     assertVectorsAlmostEqual(values.real.p_eff, values.test.p_eff, 'relative', 0.4);        
         
         
 	function testPeffLatent
     values.real = load('realValuesModelAssesment2.mat', 'p_eff_latent');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'p_eff_latent');
+    values.real.p_eff_latent(isnan(values.real.p_eff_latent)) = 0;
+     values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'p_eff_latent');
+     values.test.p_eff_latent(isnan(values.test.p_eff_latent)) = 0;
     assertVectorsAlmostEqual(values.real.p_eff_latent, values.test.p_eff_latent, 'relative', 0.05);
         
         
 	function testPeffLatent2
     values.real = load('realValuesModelAssesment2.mat', 'p_eff_latent2');
-    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'p_eff_latent2');
+    values.real.p_eff_latent2(isnan(values.real.p_eff_latent2)) = 0;
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'),'p_eff_latent2');
+    values.test.p_eff_latent2(isnan(values.test.p_eff_latent2)) = 0;
     assertVectorsAlmostEqual(values.real.p_eff_latent2, values.test.p_eff_latent2, 'relative', 0.25);
         
         
@@ -72,3 +84,8 @@ initTestSuite;
     values.real = load('realValuesModelAssesment2.mat', 'mlpd_cv');
     values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'mlpd_cv');
     assertVectorsAlmostEqual(values.real.mlpd_cv, values.test.mlpd_cv, 'relative', 0.05);
+    
+  function testLogPredDensityLOOPRED
+    values.real = load('realValuesModelAssesment2.mat', 'mlpd_loo');
+    values.test = load(strrep(which('test_modelassesment2.m'), 'test_modelassesment2.m', 'testValues/testModelAssesment2.mat'), 'mlpd_loo');
+    assertVectorsAlmostEqual(values.real.mlpd_loo, values.test.mlpd_loo, 'relative', 0.05);
