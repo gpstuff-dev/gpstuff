f5a84c774ea20106f9ec28203e0fc16f2eefcfee
Ville Tolvanen <ville.tolvanen@aalto.fi>
2013-03-08 14:08:53 +0200

    Added better plots to demo_passgp

--- gp/demo_passgp.m
+++ gp/demo_passgp.m
@@ -15,6 +15,7 @@
 % License.txt, included with the software, for details.
 
 % Generate toy data
+prevstream=setrandstream(0);
 [x1,x2]=meshgrid(-5:0.1:5,-5:0.1:5);
 x=[x1(:) x2(:)]; x=x(randperm(size(x,1),3000),:);
 y=2.*mnorm_pdf(x, [0 0], [0.5 0;0 0.5]) + mnorm_pdf(x, [3 3], [0.5 0;0 0.5]) + mnorm_pdf(x, [-3 -3], [0.5 0;0 0.5]);
@@ -22,10 +23,12 @@ y=y+mnorm_pdf(x, [3 -3], [0.5 0;0 0.5])+mnorm_pdf(x, [-3 3], [0.5 0;0 0.5]);
 y=y+0.03.*randn(size(y,1),1);
 y(y>0.15)=1; y(y<=0.15)=-1;
 
-xt=-4 + 8.*rand(2000,2);
-yt=2.*mnorm_pdf(xt, [0 0], [0.5 0;0 0.5]) + mnorm_pdf(xt, [3 3], [0.5 0;0 0.5]) + mnorm_pdf(xt, [-3 -3], [0.5 0;0 0.5]);
-yt=yt+mnorm_pdf(xt, [3 -3], [0.5 0;0 0.5])+mnorm_pdf(xt, [-3 3], [0.5 0;0 0.5]);
-yt(yt>0.15)=1; yt(yt<=0.15)=-1;
+[xt1, xt2]=meshgrid(-5:0.23:5,-5:0.23:5);
+xt=[xt1(:) xt2(:)];
+yt=ones(size(xt,1),1);
+% yt=2.*mnorm_pdf(xt, [0 0], [0.5 0;0 0.5]) + mnorm_pdf(xt, [3 3], [0.5 0;0 0.5]) + mnorm_pdf(xt, [-3 -3], [0.5 0;0 0.5]);
+% yt=yt+mnorm_pdf(xt, [3 -3], [0.5 0;0 0.5])+mnorm_pdf(xt, [-3 3], [0.5 0;0 0.5]);
+% yt(yt>0.15)=1; yt(yt<=0.15)=-1;
 
 [n, nin] = size(x);
 
@@ -42,6 +45,8 @@ w0=gp_pak(gp);
 start=tic;[gp, indA]=passgp(gp, x, y, 'opt', opt, 'npass', 4, 'ninit', 800, 'nsub', 10, 'display', 'on', 'fixed', 'on', 'pexc', 0.1, 'optimn', 2);time=toc(start);
 tt=time;
 [Eft, Varft, lpyt, Eyt, Varyt]=gp_pred(gp, x(indA,:), y(indA,:), xt, 'yt', yt);
+figure, [cc,hh]=contour(reshape(xt(:,1),size(xt1,1), size(xt1,1)), reshape(xt(:,2),size(xt1,1), size(xt1,1)), reshape(exp(lpyt),size(xt1,1), size(xt1,1)), [0.1 0.9]);
+clabel(cc,hh);
 param=gp_pak(gp);
 
 % PASS-GP with inclusion threshold 0.65, deletion threshold 0.99, intial
@@ -50,6 +55,8 @@ gp=gp_unpak(gp,w0);
 start=tic;[gp, indA2]=passgp(gp, x, y, 'opt', opt, 'pinc', 0.65, 'pdel', 0.99, 'npass', 3, 'ninit', 400, 'nsub', 10, 'display', 'on', 'optimn', 2);time=toc(start);
 tt2=time;
 [Eft2, Varft2, lpyt2, Eyt2, Varyt2]=gp_pred(gp, x(indA2,:), y(indA2,:), xt, 'yt', yt);
+figure, [cc,hh]=contour(reshape(xt(:,1),size(xt1,1), size(xt1,1)), reshape(xt(:,2),size(xt1,1), size(xt1,1)), reshape(exp(lpyt2),size(xt1,1), size(xt1,1)), [0.1 0.9]);
+clabel(cc,hh);
 param2=gp_pak(gp);
 
 % Full Gaussian Process for comparison
@@ -57,7 +64,8 @@ gp=gp_unpak(gp,w0);
 opt.Display='iter';
 start=tic;gp=gp_optim(gp,x,y,'opt',opt);tt3=toc;
 [Eft3, Varft3, lpyt3, Eyt3, Varyt3]=gp_pred(gp, x, y, xt, 'yt', yt);
-
+figure, [cc,hh]=contour(reshape(xt(:,1),size(xt1,1), size(xt1,1)), reshape(xt(:,2),size(xt1,1), size(xt1,1)), reshape(exp(lpyt3),size(xt1,1), size(xt1,1)), [0.1 0.9]);
+clabel(cc,hh);
 
 % Display some statistics
 
@@ -69,9 +77,10 @@ mlpd_full=mean(lpyt3)
 time_full=tt3
 
 % Plot data and active sets for both methods
-figure(1), subplot(1,2,1),  plot(x(y==1,1),x(y==1,2),'or',x(y==-1,1),x(y==-1,2),'ob'); 
+figure(4), subplot(1,2,1),  plot(x(y==1,1),x(y==1,2),'or',x(y==-1,1),x(y==-1,2),'ob'); 
 hold all; plot(x(indA,1), x(indA,2), '.k'); title('Data and active set (fpass-gp)')
 legend('y=1', 'y=-1', 'Active set for fpass-gp');
 subplot(1,2,2),  plot(x(y==1,1),x(y==1,2),'or',x(y==-1,1),x(y==-1,2),'ob'); 
 hold all; plot(x(indA2,1), x(indA2,2), '.k'); title('Data and active set (pass-gp)')
 legend('y=1', 'y=-1', 'Active set for pass-gp');
+setrandstream(0);
