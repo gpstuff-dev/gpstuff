98a1d889c9c9a7c6284459dde78e574b7b4e2ce7
Ville Tolvanen <ville.tolvanen@aalto.fi>
2013-03-11 13:14:47 +0200

    Added Octave options to startup.m

--- startup.m
+++ startup.m
@@ -2,10 +2,15 @@
 % adds subfolders automatically to the path
 F = mfilename;
 S = which(F);
-subfolders={'diag' 'dist' 'gp' 'mc' 'misc' 'optim' 'xunit'};
+if exist('OCTAVE_VERSION', 'builtin')
+  subfolders={'diag' 'dist' 'gp' 'mc' 'misc' 'optim' 'xunit', 'octave_compat', 'inputparser'};
+else
+  subfolders={'diag' 'dist' 'gp' 'mc' 'misc' 'optim' 'xunit'};
+end
 for sf=subfolders
   addpath(strrep(S,[F '.m'],sf{:}))
 end
+
 
 % Alternatively copy following lines to startup.m in MATLAB startup folder
 % and edit gpstuffroot to point where you have installed the GPstuff
@@ -17,3 +22,7 @@ end
 %addpath([gpstuffroot 'misc'])
 %addpath([gpstuffroot 'optim'])
 %addpath([gpstuffroot 'xunit'])
+
+% If using Octave version of GPstuff, also add the following
+%addpath([gpstuffroot 'inputparser'])
+%addpath([gpstuffroot 'octave_compat'])
