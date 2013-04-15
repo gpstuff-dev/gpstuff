5f3d5910251ad3dc954ee7bc1a64a4d4d9e8eb6b
Aki Vehtari <Aki.Vehtari@aalto.fi>
2013-04-15 17:19:25 +0300

    Better? weakly informative priors?

--- gp/lgpdens.m
+++ gp/lgpdens.m
@@ -398,16 +398,19 @@ function [Ef,Covf] = gpsmooth(xx,yy,xxt,gpcf,latent_method,int_method,display,sp
     gpcf1 = gpcf();
   end
   
-  % weakly informative prior
-  pm = prior_logunif();
-  pl = prior_t('s2', 10^2, 'nu', 4);
-  pa = prior_t('s2', 10^2, 'nu', 4);
+  % weakly informative priors
+  % prior based on guess of maximum differences in log densities
+  pm = prior_sqrtt('s2',10^2,'nu',4);
+  % Prior width for lengthscale using Silverman's rule of thumb of min grid distance
+  h=min(diff(xx(1:2,end)).^2,sum(yy).^-1/5);
+  pl = prior_invt('s2', 1./h, 'nu', 1);
+  pa = prior_t('s2', 20^2, 'nu', 1);
   %pm = prior_sqrtt('s2', 10^2, 'nu', 4);
-  %pl = prior_t('s2', 1^2, 'nu', 4);
+  %pl = prior_sinvchi2('s2', 1, 'nu', 1);
   %pa = prior_t('s2', 10^2, 'nu', 4);
   % different covariance functions have different parameters
   if isfield(gpcf1,'magnSigma2')
-     gpcf1 = gpcf(gpcf1, 'magnSigma2', .5, 'magnSigma2_prior', pm);
+     gpcf1 = gpcf(gpcf1, 'magnSigma2', 1, 'magnSigma2_prior', pm);
   end
   if isfield(gpcf1,'lengthScale')
      gpcf1 = gpcf(gpcf1, 'lengthScale', .5, 'lengthScale_prior', pl);
@@ -416,7 +419,7 @@ function [Ef,Covf] = gpsmooth(xx,yy,xxt,gpcf,latent_method,int_method,display,sp
     gpcf1 = gpcf(gpcf1, 'alpha', 20, 'alpha_prior', pa);
   end
   if isfield(gpcf1,'biasSigma2')
-    gpcf1 = gpcf(gpcf1, 'biasSigma2', 10, 'weightSigma2', 10,'biasSigma2_prior',prior_logunif(),'weightSigma2_prior',prior_logunif());
+    gpcf1 = gpcf(gpcf1, 'biasSigma2', 10, 'weightSigma2', 10,'biasSigma2_prior',prior_sinvchi2('s2',1^2,'nu',1),'weightSigma2_prior',prior_sinvchi2('s2',1^2,'nu',1));
   end
   
   if ~isempty(cond_dens) && strcmp(cond_dens, 'on')
@@ -430,7 +433,6 @@ function [Ef,Covf] = gpsmooth(xx,yy,xxt,gpcf,latent_method,int_method,display,sp
   if ~isempty(basis_function) && strcmp(basis_function, 'off')
     gp = gp_set('lik', lik, 'cf', {gpcf1}, 'jitterSigma2', 1e-4);
   else
-    %gpmfco = gpmf_constant('prior_mean',0,'prior_cov',100);
     gpmflin = gpmf_linear('prior_mean',0,'prior_cov',100);
     gpmfsq = gpmf_squared('prior_mean',0,'prior_cov',100);
     gp = gp_set('lik', lik, 'cf', {gpcf1}, 'jitterSigma2', 1e-4, 'meanf', {gpmflin,gpmfsq});
@@ -457,9 +459,10 @@ function [Ef,Covf] = gpsmooth(xx,yy,xxt,gpcf,latent_method,int_method,display,sp
       gp.latent_opt.fft=1;
     end
   end
-  
+
   gp=gp_optim(gp,xx,yy,'opt',opt, 'optimf', @fminlbfgs);
   %gradcheck(gp_pak(gp), @gpla_nd_e, @gpla_nd_g, gp, xx, yy);
+  %gp_pak(gp)
   
   if strcmpi(latent_method,'MCMC')
     gp = gp_set(gp, 'latent_method', 'MCMC');
