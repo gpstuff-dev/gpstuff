25d7806e3a0037c582da73f2bde1b3dc44c6f151
Aki Vehtari <Aki.Vehtari@aalto.fi>
2013-07-03 20:31:55 +0300

    Works now for matrices, and accepts optional second argument DIM.

--- misc/sumlogs.m
+++ misc/sumlogs.m
@@ -1,17 +1,23 @@
 function y = sumlogs(x)
-%SUMLOGS Sum of vector where numbers are represented by their logarithms.
+%SUMLOGS Sum of elements where numbers are represented by their logarithms.
 %
 %  Description
-%    C=ADDLOGS(A) computes C=log(sum(exp(A))) in such a fashion
+%    C=SUMLOGS(A) computes C=log(sum(exp(A))) in such a fashion
 %    that it works even when elements have large magnitude.
+%
+%    C=SUMLOGS(A,DIM) sums along the dimension DIM. 
+%
+%    See also SUM
+%
 
-% Copyright (c) 2003 Aki Vehtari
+% Copyright (c) 2013 Aki Vehtari
 
 % This software is distributed under the GNU General Public
 % License (version 3 or later); please refer to the file
 % License.txt, included with the software, for details.
 
-y=x(1);
-for k=2:length(x)
-  y=addlogs(y,x(k));
+if nargin<2
+  dim=find(size(x)>1,1);
 end
+maxx=max(x(:));
+y=maxx+log(sum(exp(x-maxx),dim));
