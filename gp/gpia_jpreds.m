64a55c2ea6fac88e31d803cc04f6a2e6e63c94f6
Aki Vehtari <Aki.Vehtari@aalto.fi>
2013-08-23 14:07:59 +0300

    New function gpia_jpreds

---
+++ gp/gpia_jpreds.m
@@ -0,0 +1,153 @@
+function [Eft, Covft] = gpia_jpreds(gp_array, x, y, varargin)
+%GPIA_JPREDS  Predictions with Gaussian Process GP_IA solution.
+%
+%  Description
+%    [EFS, COVFS] = GPIA_JPREDS(GP_ARRAY, X, Y, XT, OPTIONS) 
+%    takes a cell array of GP structures together with matrix X of
+%    training inputs and vector Y of training targets, and
+%    evaluates the predictive distribution at test inputs XT with
+%    parameters marginalized out with IA. Returns
+%    matrices EFS and COVFS that contain means and covariances of the
+%    conditional posterior predictive distributions.
+%
+%    OPTIONS is optional parameter-value pair
+%      predcf - index vector telling which covariance functions are 
+%               used for prediction. Default is all (1:gpcfn). See
+%               additional information below.
+%      tstind - a vector/cell array defining, which rows of X belong 
+%               to which training block in *IC type sparse models. 
+%               Deafult is []. In case of PIC, a cell array
+%               containing index vectors specifying the blocking
+%               structure for test data. IN FIC and CS+FIC a vector
+%               of length n that points out the test inputs that
+%               are also in the training set (if none, set TSTIND=[])
+%      yt     - optional observed yt in test points (see below)
+%      z      - optional observed quantity in triplet (x_i,y_i,z_i)
+%               Some likelihoods may use this. For example, in case
+%               of Poisson likelihood we have z_i=E_i, that is,
+%               expected value for ith case.
+%      zt     - optional observed quantity in triplet (xt_i,yt_i,zt_i)
+%               Some likelihoods may use this. For example, in case
+%               of Poisson likelihood we have z_i=E_i, that is, the
+%               expected value for the ith case.
+%       
+%    NOTE! In case of FIC and PIC sparse approximation the
+%    prediction for only some PREDCF covariance functions is just
+%    an approximation since the covariance functions are coupled in
+%    the approximation and are not strictly speaking additive
+%    anymore.
+%
+%    For example, if you use covariance such as K = K1 + K2 your
+%    predictions Eft1 = gpia_pred(gp_array, X, Y, X, 'predcf', 1) and
+%    Eft2 = gpia_pred(gp_array, x, y, x, 'predcf', 2) should sum up to
+%    Eft = gpia_pred(gp_array, x, y, x). That is Eft = Eft1 + Eft2. With
+%    FULL model this is true but with FIC and PIC this is true only
+%    approximately. That is Eft \approx Eft1 + Eft2.
+%
+%    With CS+FIC the predictions are exact if the PREDCF covariance
+%    functions are all in the FIC part or if they are CS
+%    covariances.
+%
+%    NOTE! When making predictions with a subset of covariance
+%    functions with FIC approximation the predictive variance can
+%    in some cases be ill-behaved i.e. negative or unrealistically
+%    small. This may happen because of the approximative nature of
+%    the prediction.
+%
+%  See also
+%    GP_PRED, GP_SET, GP_IA
+%
+  
+% Copyright (c) 2009 Ville Pietil�inen
+% Copyright (c) 2009-2010 Jarno Vanhatalo    
+% Copyright (c) 2011-2012 Ville Tolvanen
+% Copyright (c) 2012-2013 Aki Vehtari
+
+% This software is distributed under the GNU General Public 
+% Licence (version 3 or later); please refer to the file 
+% Licence.txt, included with the software, for details.    
+
+  
+  ip=inputParser;
+  ip.FunctionName = 'GPIA_JPREDS';
+  ip.addRequired('gp_array', @iscell);
+  ip.addRequired('x', @(x) ~isempty(x) && isreal(x) && all(isfinite(x(:))))
+  ip.addRequired('y', @(x) ~isempty(x) && isreal(x) && all(isfinite(x(:))))
+  ip.addOptional('xt', [], @(x) isempty(x) || (isreal(x) && all(isfinite(x(:)))))
+  ip.addParamValue('yt', [], @(x) isreal(x) && all(isfinite(x(:))))
+  ip.addParamValue('z', [], @(x) isreal(x) && all(isfinite(x(:))))
+  ip.addParamValue('zt', [], @(x) isreal(x) && all(isfinite(x(:))))
+  ip.addParamValue('predcf', [], @(x) isempty(x) || ...
+                   isvector(x) && isreal(x) && all(isfinite(x)&x>0))
+  ip.addParamValue('tstind', [], @(x) isempty(x) || iscell(x) ||...
+                   (isvector(x) && isreal(x) && all(isfinite(x)&x>0)))
+  if numel(varargin)==0 || isnumeric(varargin{1})
+    % inputParser should handle this, but it doesn't
+    ip.parse(gp_array, x, y, varargin{:});
+  else
+    ip.parse(gp_array, x, y, [], varargin{:});
+  end
+  xt=ip.Results.xt;
+  yt=ip.Results.yt;
+  z=ip.Results.z;
+  zt=ip.Results.zt;
+  predcf=ip.Results.predcf;
+  tstind=ip.Results.tstind;
+  if isempty(xt)
+    xt=x;
+    if isempty(tstind)
+      if iscell(gp_array)
+        gptype=gp_array{1}.type;
+      else
+        gptype=gp_array.type;
+      end
+      switch gptype
+        case {'FULL' 'VAR' 'DTC' 'SOR'}
+          tstind = [];
+        case {'FIC' 'CS+FIC'}
+          tstind = 1:size(x,1);
+        case 'PIC'
+          if iscell(gp)
+            tstind = gp_array{1}.tr_index;
+          else
+            tstind = gp_array.tr_index;
+          end
+      end
+    end
+    if isempty(yt)
+      yt=y;
+    end
+    if isempty(zt)
+      zt=z;
+    end
+  end
+  
+  % pass these forward
+  options=struct();
+  if ~isempty(yt);options.yt=yt;end
+  if ~isempty(z);options.z=z;end
+  if ~isempty(zt);options.zt=zt;end
+  if ~isempty(predcf);options.predcf=predcf;end
+  if ~isempty(tstind);options.tstind=tstind;end
+  
+  if nargout > 2 && isempty(yt)
+    pyt = NaN;
+  end
+  
+  nGP = numel(gp_array);
+  
+  for i=1:nGP
+    P_TH(i,:) = gp_array{i}.ia_weight;
+  end
+
+  % Make predictions with different models in gp_array
+  for j = 1:nGP
+    if isempty(yt)
+      [Eft_grid(j,:), Covft_grid(:,:,j)]=gp_jpred(gp_array{j},x,y,xt,options);            
+    else
+      [Eft_grid(j,:), Covft_grid(:,:,j), ljpyt_grid(j), Eyt_grid(j,:), Covyt_grid(:,:,j)]=gp_jpred(gp_array{j},x,y,xt, options);
+    end
+  end
+  
+  Eft=Eft_grid;
+  Covft=Covft_grid;
