e31b491df1c2ed670c063814604ee01a9613ab3b
Aki Vehtari <Aki.Vehtari@aalto.fi>
2014-04-17 16:31:03 +0300

    Few forgotten GPL 2->3

--- gp/lik_inputdependentnoise.m
+++ gp/lik_inputdependentnoise.m
@@ -37,7 +37,7 @@ function lik = lik_inputdependentnoise(varargin)
 % Copyright (c) 2011 Ville Tolvanen
 
 % This software is distributed under the GNU General Public
-% License (version 2 or later); please refer to the file
+% License (version 3 or later); please refer to the file
 % License.txt, included with the software, for details.
 
   ip=inputParser;
